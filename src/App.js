a989d8ba1b100313e86a4a1641ec6b43440467a8
seonyo <jsy6535@naver.com>
2024-05-29 08:26:53 +0900

    [Feat] (#1) 메인화면 라우터 연결

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React from "react";
+
+import MainPage from './pages/main/main-page';
+
 function App() {
   return (
-    <></>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
