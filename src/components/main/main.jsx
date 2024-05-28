22c4ef89fdedb7ee327713d8e4345ece3521ef34
seonyo <jsy6535@naver.com>
2024-05-29 08:32:59 +0900

    [Feat] (#1) 버튼 링크 연결

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "../../css/comm/all.css"
 import styles from "../../css/main/main.module.css"
 
-const Main = () =>{
-    return(
+const Main = () => {
+    return (
         <div className={styles.container}>
             <div className={styles.title}>
-                <img src={`${process.env.PUBLIC_URL}/images/main/title.svg`}/>
+                <img src={`${process.env.PUBLIC_URL}/images/main/title.svg`} />
             </div>
             <div className={styles.btn}>
-                <img src={`${process.env.PUBLIC_URL}/images/main/ranking-btn.png`}/>
-                <img src={`${process.env.PUBLIC_URL}/images/main/gamestart-btn.svg`}/>
+                <Link to='/rankig'><img src={`${process.env.PUBLIC_URL}/images/main/ranking-btn.png`} /></Link>
+                <Link to='/nickname'><img src={`${process.env.PUBLIC_URL}/images/main/gamestart-btn.svg`} /></Link>
             </div>
         </div>
     )
