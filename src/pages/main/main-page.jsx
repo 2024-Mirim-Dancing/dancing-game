5246f54a64b15e4d17efc012de8eb723df1a1764
seonyo <jsy6535@naver.com>
2024-05-29 08:26:34 +0900

    [Feat] (#1) 메인화면 페이지 구현

---
+++ src/pages/main/main-page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "../../components/main/main";
+
+const MainPage = () => {
+    return (
+        <>
+            <Main />
+
+        </>
+    )
+}
+
+export default MainPage;
