edd7de6ef71c7aa3a95ae3045e35121ff651d6a5
seonyo <jsy6535@naver.com>
2024-05-29 14:56:59 +0900

    [Feat] (#4) teacher 페이지 구현

---
+++ src/pages/teacher/teacher-page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Teacher from "../../components/teacher/teacher";
+
+const TeacherPage = () => {
+    return (
+        <>
+            <Teacher />
+        </>
+    )
+}
+
+export default TeacherPage;
