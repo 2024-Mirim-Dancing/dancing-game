133bf1748b321e18cdebf2dde505e8fee877e6b6
seonyo <jsy6535@naver.com>
2024-05-29 16:48:19 +0900

    [Feat] (#3) 뒤로가기 기능 추가

--- src/components/ranking/ranking.jsx
+++ src/components/ranking/ranking.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 
+import { useNavigate } from "react-router-dom";
+
 import styles from "../../css/ranking/ranking.module.css"
 import RankingRow from "./ranking-row";
+
+
 const Ranking = () => {
+    const navigate = useNavigate();
+
     const rankingData = [
         { name: "박화경상남도독도", score: 103645545656 },
         { name: "정선영구업서예~", score: 95 },
         { name: "임지현", score: 90 }
     ];
 
+    const handleBackArrow = () =>{
+        navigate(-1);
+    }
+
     return (
         <div className={styles.container}>
-            <div className={styles.arrow}>
+            <div className={styles.arrow} onClick={handleBackArrow}>
                 <img src={`${process.env.PUBLIC_URL}/images/ranking/pre-arrow.svg`} />
             </div>
             <div className={styles.title}>
