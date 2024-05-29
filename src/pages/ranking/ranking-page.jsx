c1bf5e89da93bd578ca42558bcf9319478ab3d10
seonyo <jsy6535@naver.com>
2024-05-29 16:45:01 +0900

    [Feat] (#3) ranking 페이지 구현

---
+++ src/pages/ranking/ranking-page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Ranking from "../../components/ranking/ranking";
+
+const RankingPage = () =>{
+    return(
+        <Ranking/>
+    )
+}
+
+export default RankingPage;
