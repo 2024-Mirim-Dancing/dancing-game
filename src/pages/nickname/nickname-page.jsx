26bd7a09446c3e449334a68522bcc821473cd8d0
seonyo <jsy6535@naver.com>
2024-05-29 09:54:04 +0900

    [Feat] (#2) next-btn 페이지 추가

--- src/pages/nickname/nickname-page.jsx
+++ src/pages/nickname/nickname-page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Nickname from "../../components/nickname/nickname";
 
+import NextBtn from "../../components/comm/next-btn";
 const MainPage = () => {
     return (
         <>
             <Nickname />
+            <NextBtn/>
         </>
     )
 }
