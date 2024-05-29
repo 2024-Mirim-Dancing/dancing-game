de6df9306fcfa879f899aed6608b34d6970dccb7
seonyo <jsy6535@naver.com>
2024-05-29 09:44:34 +0900

    [Feat] (#2) Nickname 페이지 처리

---
+++ src/pages/nickname/nickname-page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nickname from "../../components/nickname/nickname";
+
+const MainPage = () => {
+    return (
+        <>
+            <Nickname />
+        </>
+    )
+}
+
+export default MainPage;
