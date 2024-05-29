918097463f260680ac07fcfd89b71967f7d2a6eb
seonyo <jsy6535@naver.com>
2024-05-29 09:44:50 +0900

    [Feat] (#2) Nickname 입력 화면 라우터 등록

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import React from "react";
 
 import MainPage from './pages/main/main-page';
+import NicknamePage from './pages/nickname/nickname-page';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<MainPage />} />
+        <Route path='/nickname' element={<NicknamePage />} />
       </Routes>
     </Router>
   )
