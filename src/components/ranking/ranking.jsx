71c8f17fecad534a4a2a808d37ed4e9accda813a
seonyo <jsy6535@naver.com>
2024-05-29 16:43:12 +0900

    [Feat] (#3) ranking 컴포넌트 구현

---
+++ src/components/ranking/ranking.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import styles from "../../css/ranking/ranking.module.css"
+import RankingRow from "./ranking-row";
+const Ranking = () => {
+    const rankingData = [
+        { name: "박화경상남도독도", score: 103645545656 },
+        { name: "정선영구업서예~", score: 95 },
+        { name: "임지현", score: 90 }
+    ];
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.arrow}>
+                <img src={`${process.env.PUBLIC_URL}/images/ranking/pre-arrow.svg`} />
+            </div>
+            <div className={styles.title}>
+                <img src={`${process.env.PUBLIC_URL}/images/ranking/title.svg`} />
+            </div>
+            <div className={styles.content}>
+                {rankingData.map((entry, index) => (
+                    <RankingRow className={styles.ranking_row}
+                    key={index} ranking={index + 1} name={entry.name} score={entry.score} />
+                ))}
+            </div>
+
+        </div>
+    )
+}
+
+export default Ranking;
