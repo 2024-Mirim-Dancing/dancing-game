ae1fb3e3582d40cc3c459e9282f552a4076ee15f
seonyo <jsy6535@naver.com>
2024-05-29 16:45:07 +0900

    [Feat] (#3) ranking 라우터 처리

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import MainPage from './pages/main/main-page';
 import NicknamePage from './pages/nickname/nickname-page';
 import TeacherPage from './pages/teacher/teacher-page';
+import RankingPage from './pages/ranking/ranking-page';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<MainPage />} />
         <Route path='/nickname' element={<NicknamePage />} />
         <Route path='/teacher' element={<TeacherPage />} />
+        <Route path='/ranking' element={<RankingPage/>} />
       </Routes>
     </Router>
   )
