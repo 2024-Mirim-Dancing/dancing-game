08206703cf8c4ff71f20a347e21d1b4f26337523
seonyo <jsy6535@naver.com>
2024-06-12 03:23:12 +0900

    Feat: 게임 플레이 시 bgm 나오는 기능 추가

--- src/pages/game/start.jsx
+++ src/pages/game/start.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import Start from "../../components/game/start";
 import Timebar from "../../components/game/timebar";
 
 const StartPage = () => {
+    const audioRef = useRef(null);
+
+    useEffect(() => {
+        if (audioRef.current) {
+            audioRef.current.play();
+        }
+    }, []);
+
     return (
         <>
             <Start />
             <Timebar />
-
+            <audio ref={audioRef} src={`${process.env.PUBLIC_URL}/sound/몽키매직.wav`} loop />
         </>
     )
 }
