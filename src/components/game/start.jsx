450f41a231df7053171a777c5900a66225f11b00
seonyo <jsy6535@naver.com>
2024-06-12 02:45:54 +0900

    Feat: start 컴포넌트 생성

---
+++ src/components/game/start.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "../../css/game/start.module.css"
+const Start = () =>{
+    return(
+        <div className={styles.container}>
+        </div>
+    )
+}
+
+export default Start;
