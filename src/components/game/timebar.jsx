17350cbd392c5f265c6dfd6b8652ca9bb95a410e
seonyo <jsy6535@naver.com>
2024-06-12 03:45:50 +0900

    Fix: 게이지가 다 안줄었는데 게임 종료되는 버그 해결

--- src/components/game/timebar.jsx
+++ src/components/game/timebar.jsx
@@ -5,26 +5,55 @@ import ProgressBar from "@ramonak/react-progress-bar";
 import styles from "../../css/game/timebar.module.css"
 const Timebar = () => {
     const [completed, setCompleted] = useState(100);
+    const [spacePressed, setSpacePressed] = useState(false);
 
     const navigate = useNavigate();
-
     useEffect(() => {
-
         const interval = setInterval(() => {
-            setCompleted(prevCompleted => {
-                const newCompleted = prevCompleted - 5;
-                return newCompleted > 0 ? newCompleted : 0;
-            });
+            if (spacePressed) {
+                setCompleted(prevCompleted => {
+                    const newCompleted = prevCompleted + 8;
+                    return newCompleted > 100 ? 100 : newCompleted;
+                });
+            } else {
+                setCompleted(prevCompleted => {
+                    const newCompleted = prevCompleted - 2;
+                    return newCompleted > 0 ? newCompleted : 0;
+                });
+            }
         }, 500);
 
         return () => clearInterval(interval);
-    }, []);
+    }, [spacePressed]);
 
     useEffect(() => {
+        console.log(completed)
         if (completed <= 0) {
             navigate("/end");
         }
     }, [completed, navigate]);
+
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.code === "Space") {
+                setSpacePressed(true);
+            }
+        };
+
+        const handleKeyUp = (event) => {
+            if (event.code === "Space") {
+                setSpacePressed(false);
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+        window.addEventListener("keyup", handleKeyUp);
+
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+            window.removeEventListener("keyup", handleKeyUp);
+        };
+    }, []);
 
     return (
         <>
