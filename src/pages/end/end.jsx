6664c1ab818d4f02b2031707198b86b315c78217
seonyo <jsy6535@naver.com>
2024-06-14 04:30:03 +0900

    Feat: 게임 종료 페이징 처리

---
+++ src/pages/end/end.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import End from "../../components/end/end"
+
+const EndPage = () => {
+    return (
+        <>
+            <End />
+        </>
+    )
+}
+
+export default EndPage;
