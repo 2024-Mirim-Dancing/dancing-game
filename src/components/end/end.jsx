076e12c0804752cea48e5655fdf1f3f2de55b9b6
seonyo <jsy6535@naver.com>
2024-06-14 10:13:41 +0900

    Feat: 게임 종료 화면 완성

--- src/components/end/end.jsx
+++ src/components/end/end.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-
 
 import "../../css/comm/all.css"
 import styles from "../../css/end/end.module.css"
@@ -8,16 +7,53 @@ import styles from "../../css/end/end.module.css"
 const End = () => {
     const location = useLocation();
     const { score } = location.state || { score: 0 };
+    const { teacherName } = location.state || { teacherName: "" };
+    const [comment, setComment] = useState("");
+
+    useEffect(() => {
+        switch (teacherName) {
+            case "SMS":
+                setComment("칠판 보세요 칠판~");
+                break;
+            case "KYC":
+                setComment("춤 잘 추는데 자퇴하고 아이돌이나 한 번 해봐~");
+                break;
+            case "KYH":
+                setComment("어이 크라이 다메다로");
+                break;
+            case "KJS":
+                setComment("여기봐 어디보니 집중해라");
+                break;
+            case "KHS":
+                setComment("계속해봐 멋있다.");
+                break;
+            case "YBS":
+                setComment("좀 더 집중해주세요");
+                break;
+            default:
+                setComment("");
+        }
+    }, [teacherName]);
+
+    const teacherClass = teacherName === "SMS" ? styles.teacherSms : styles.teacher;
 
     return (
         <div className={styles.container}>
             <div className={styles.title}>당신의 점수는?</div>
-            <div className={styles.score}>{score}점</div>
-            <div className={styles.line}>
-            <img src={`${process.env.PUBLIC_URL}/images/end/line.svg`} alt="Student" />
+            <div className={styles.scoreBox}>
+                <div className={styles.score}>{score}점</div>
+                <div className={styles.line}>
+                    <img src={`${process.env.PUBLIC_URL}/images/end/line.svg`} alt="line" />
+                </div>
+            </div>
+            <div className={styles.comment}>
+                {comment}
+            </div>
+            <div className={teacherClass}>
+                <img src={`${process.env.PUBLIC_URL}/images/game/${teacherName}_1.svg`} alt={teacherName} />
             </div>
         </div>
-    )
+    );
 }
 
 export default End;
