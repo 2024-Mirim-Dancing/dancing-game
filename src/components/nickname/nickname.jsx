bf22d9591c4c74d213e3f510b2575344210b1ea6
seonyo <jsy6535@naver.com>
2024-06-15 23:27:05 +0900

    Feat: 닉네임 등록 시 로컬스토리지 저장

--- src/components/nickname/nickname.jsx
+++ src/components/nickname/nickname.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
 
 import "../../css/comm/all.css"
 import styles from "../../css/nickname/nickname.module.css"
 const Nickname = () => {
+    const navigate = useNavigate();
+    const [name, setName] = useState("");
+ 
+    const handleNextBtn = () => {
+        console.log(name)
+        localStorage.setItem("nickname", name);
+        navigate("/teacher");
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.title}>
@@ -10,7 +20,12 @@ const Nickname = () => {
             </div>
             <div className={styles.input_container}>
                 <img src={`${process.env.PUBLIC_URL}/images/nickname/input.svg`} />
-                <input type="text" className={styles.inputbox}></input>
+                <input type="text" 
+                className={styles.inputbox}
+                onChange={(e) => setName(e.target.value)}></input>
+            </div>
+            <div className={styles.next_btn}>
+                <img src={`${process.env.PUBLIC_URL}/images/comm/next-btn.svg`} onClick={handleNextBtn} />
             </div>
         </div>
     )
--- src/pages/nickname/nickname-page.jsx
+++ src/pages/nickname/nickname-page.jsx
@@ -6,7 +6,6 @@ const MainPage = () => {
     return (
         <>
             <Nickname />
-            <NextBtn/>
         </>
     )
 }
