e80c478e30d659ed7b88760b811e6be5caa739b7
mic050r <s2216@e-mirim.hs.kr>
2024-06-18 21:45:37 +0900

    Feat: 랭킹 추가 API 연동 완료

--- src/components/end/end.jsx
+++ src/components/end/end.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios"; // Axios import
 
-import "../../css/comm/all.css"
-import styles from "../../css/end/end.module.css"
+import "../../css/comm/all.css";
+import styles from "../../css/end/end.module.css";
 
 const End = () => {
     const location = useLocation();
@@ -10,7 +11,9 @@ const End = () => {
     const { score } = location.state || { score: 0 };
     const { teacherName } = location.state || { teacherName: "" };
     const [comment, setComment] = useState("");
-
+    const nickname = localStorage.getItem("nickname"); // LocalStorage에서 닉네임 불러오기
+    
+    // 선생님 한국어 이름에 따라 코멘트 설정
     useEffect(() => {
         switch (teacherName) {
             case "SMS":
@@ -36,15 +39,42 @@ const End = () => {
         }
     }, [teacherName]);
 
+    // 점수 제출 함수
+    useEffect(() => {
+        const submitScore = async () => {
+            if (teacherName && score) {
+                if (teacherName) {
+                    try {
+                        // Axios를 사용하여 점수 제출 요청
+                        const response = await axios.post(
+                            `http://127.0.0.1:3001/score/${nickname}/${score}/${teacherName}`
+                        );
+                        console.log(response.data.message); // 성공 메시지 출력
+                    } catch (error) {
+                        // 오류 처리
+                        console.error("Error response:", error.response);
+                        console.error("Error message:", error.message);
+                    }
+                }
+            }
+        };
+
+        submitScore(); // 함수 호출
+
+    }, [score, teacherName]);
+
+    // 랭킹 보기 버튼 핸들러
     const handleRankingbtn = () => {
-        navigate('/ranking');
-    }
+        navigate("/ranking");
+    };
 
+    // 메인으로 버튼 핸들러
     const handleMainBtn = () => {
-        console.log("SFdfda");
-        navigate('/');
-    }
+        console.log("메인으로 이동");
+        navigate("/");
+    };
 
+    // 선생님 스타일 클래스 설정
     const teacherClass = teacherName === "SMS" ? styles.teacherSms : styles.teacher;
 
     return (
@@ -53,24 +83,40 @@ const End = () => {
             <div className={styles.scoreBox}>
                 <div className={styles.score}>{score}점</div>
             </div>
-            <div className={styles.comment}>
-                {comment}
-            </div>
+            <div className={styles.comment}>{comment}</div>
             <div className={teacherClass}>
-                <img src={`${process.env.PUBLIC_URL}/images/game/${teacherName}_1.svg`} />
+                {/* 선생님 이미지 출력 */}
+                <img
+                    src={`${process.env.PUBLIC_URL}/images/game/${teacherName}_1.svg`}
+                    alt={`${teacherName}`}
+                />
             </div>
             <div className={styles.btn}>
-                <div className={styles.rankingBtn} onClick={handleRankingbtn}>랭킹보기</div>
-                <div className={styles.rankingBtnLine}>
-                    <img src={`${process.env.PUBLIC_URL}/images/end/rankingBtnLine.svg`} />
+                {/* 랭킹 보기 버튼 */}
+                <div className={styles.rankingBtn} onClick={handleRankingbtn}>
+                    랭킹보기
                 </div>
-                <div className={styles.mainBtn} onClick={handleMainBtn}>메인으로</div>
+                {/* 랭킹 버튼 라인 이미지 */}
+                <div className={styles.rankingBtnLine}>
+                    <img
+                        src={`${process.env.PUBLIC_URL}/images/end/rankingBtnLine.svg`}
+                        alt="랭킹 버튼 라인"
+                    />
+                </div>
+                {/* 메인으로 버튼 */}
+                <div className={styles.mainBtn} onClick={handleMainBtn}>
+                    메인으로
+                </div>
+                {/* 메인 버튼 라인 이미지 */}
                 <div className={styles.mainBtnLine}>
-                    <img src={`${process.env.PUBLIC_URL}/images/end/mainBtnLine.svg`} />
+                    <img
+                        src={`${process.env.PUBLIC_URL}/images/end/mainBtnLine.svg`}
+                        alt="메인 버튼 라인"
+                    />
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default End;
