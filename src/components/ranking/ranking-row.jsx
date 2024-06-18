0e7b01393dda9160568150952c15843acade6e38
mic050r <s2216@e-mirim.hs.kr>
2024-06-18 21:58:40 +0900

    Feat: 랭킹 불러오기 API 연동 완료

--- src/components/ranking/ranking-row.jsx
+++ src/components/ranking/ranking-row.jsx
@@ -2,16 +2,15 @@ import React from "react";
 
 import styles from "../../css/ranking/ranking-row.module.css"
 const RankingRow = (props) => {
-    const { ranking, name, score,teacher } = props;
+    const { ranking, name, score, teacher } = props;
 
     //이미지 저장명은 선생님 이니셜로 되어있음
-
     return (
         <div className={styles.container}>
             <img className={styles.background} src={`${process.env.PUBLIC_URL}/images/ranking/row-background.svg`} />
             <img className={styles.score_background} src={`${process.env.PUBLIC_URL}/images/ranking/score-background.svg`} />
             <p className={styles.ranking}>{ranking}</p>
-            <img className={styles.teacher} src={`${process.env.PUBLIC_URL}/images/ranking/SMS.svg`}/>
+            <img className={styles.teacher} src={`${process.env.PUBLIC_URL}/images/ranking/${teacher}.svg`}/>
             <p className={styles.name}>{name}</p>
             <p className={styles.score}>{score}점</p>
             <img src=""></img>
